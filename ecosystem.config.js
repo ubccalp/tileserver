6e38f9fcb0441cfc098217ace84e71e96755044b
joeyklee <j@jk-lee.com>
2018-03-20 10:33:09 -0400

    added ecosystem.config.js - need to input specific IP address for project on deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  /**
+   * Aplication configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'tileserver-calp',
+      script    : 'server.js',
+      env: {
+        COMMON_VARIABLE: 'true',
+        IPADDRESS:''
+      },
+      env_production : {
+        NODE_ENV: 'production',
+        IPADDRESS:''
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
